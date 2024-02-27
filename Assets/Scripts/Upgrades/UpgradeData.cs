102693ae2fac807d684adbb3c79fed27cb80a686
Joshua Torrington-Smith <59626141+CatSandwich@users.noreply.github.com>
2024-02-27 15:27:27 -0500

    dasfgasd

--- Assets/Scripts/Upgrades/UpgradeData.cs
+++ Assets/Scripts/Upgrades/UpgradeData.cs
@@ -6,7 +6,8 @@ namespace Upgrades
     public class UpgradeData<T> : UpgradeData
     {
         public override string CurrentName => Upgrades[Level].Name;
-        public override string NextName => Upgrades[Helpers.AssertRange(Level + 1, 0, MaxLevel, MaxLevel)].Name;
+        public override string NextName => NextUpgrade.Name;
+        public override int NextCost => NextUpgrade.Cost;
         public override bool IsMaxLevel => Level == MaxLevel;
         public override int MaxLevel => Upgrades.Length - 1;
         public T CurrentValue => Upgrades[Level].Value;
@@ -16,6 +17,8 @@ namespace Upgrades
             get => PlayerPrefs.GetInt(Key, 0);
             set => PlayerPrefs.SetInt(Key, Helpers.AssertRange(value, 0, Upgrades.Length - 1, 0));
         }
+
+        private Upgrade NextUpgrade => Upgrades[Helpers.AssertRange(Level + 1, 0, MaxLevel, MaxLevel)];
 
         [field: SerializeField]
         public override string Name { get; protected set; }
@@ -34,6 +37,9 @@ namespace Upgrades
 
             [field: SerializeField]
             public T Value { get; private set; }
+
+            [field: SerializeField]
+            public int Cost { get; private set; }
         }
     }
 
@@ -43,6 +49,7 @@ namespace Upgrades
         public abstract string Key { get; protected set; }
         public abstract string CurrentName { get; }
         public abstract string NextName { get; }
+        public abstract int NextCost { get; }
         public abstract int Level { get; set; }
         public abstract int MaxLevel { get; }
         public abstract bool IsMaxLevel { get; }
