d245fb232ec9ebe763c30395ebcc111b591c60f2
5zu33 <5zu33@CA203D20>
2024-02-28 11:10:06 -0500

    Created startup/audio scene, folder maintanance

---
+++ Assets/Scripts/StartupLoad.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupLoad : MonoBehaviour
+{
+    [SerializeField]
+    private string GameSceneName;
+    [SerializeField]
+    private string AudioSceneName;
+    [SerializeField]
+    private StartupFormat startupText;
+
+    const string StartupSceneName = "Startup";
+
+    private void Start()
+    {
+        //Wait for audio to load and then play intro.
+        AsyncOperation asyncScene = SceneManager.LoadSceneAsync(AudioSceneName, LoadSceneMode.Additive);
+        asyncScene.completed += (AsyncOperation operation) => { startupText.Startup(LoadMain); };
+    }
+
+    private void LoadMain()
+    {
+        //Don't wait for callbacks or anything.
+        SceneManager.UnloadSceneAsync(StartupSceneName);
+        SceneManager.LoadSceneAsync(GameSceneName, LoadSceneMode.Additive);
+    }
+}
